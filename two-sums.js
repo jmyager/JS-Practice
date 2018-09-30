fe021dabdc36985e006234028cd339ee7b2fb57b
jmyager <jmyager6@gmail.com>
2018-09-30 17:38:51 -0400

    two-sums solved

---
+++ two-sums.js
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/two-sum/description/
+
+const nums=[2,7,11,15];
+const target = 9;
+
+var twoSum = function(nums, target) {
+    for (var i = 0; i < nums.length; i++) {
+        for (var j = i + 1; j < nums.length; j++) { 
+            if (nums[i] + nums[j] === target) {
+            const array = [i,j];
+            return array;
+            }
+        }
+    }
+};
+
+twoSum(nums, target);
